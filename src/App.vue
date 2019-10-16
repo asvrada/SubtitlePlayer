8cbb07a5f00549266de681738b6819403cbeb54f
Zijie Wu <noojeff@gmail.com>
2019-10-16 11:48:59 -0400

    Revert "Added tracking"
    This reverts commit 6a95eecbf39181579aa7e5c74cb683345cfe7608.

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,7 @@
                 alt="Fork me on GitHub"></a>
 
         <div id="padding"></div>
-
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114654492-4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-114654492-4');
-</script>  </div>
+    </div>
 </template>
 
 <script>
